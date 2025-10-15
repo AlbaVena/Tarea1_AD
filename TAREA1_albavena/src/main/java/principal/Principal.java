2f8168f4e029a95af646d2337c2d2009f5c57fa8
Albav <Albav@AlbitaTeComputa.home>
2025-10-15 13:16:17 +0200

    Menus provisionales

--- TAREA1_albavena/src/main/java/principal/Principal.java
+++ TAREA1_albavena/src/main/java/principal/Principal.java
@@ -21,6 +21,7 @@ public class Principal {
 		/**
 		 * 1. ver espectaculos 2. Log IN 3. Salir
 		 */
+		mostrarMenuSesion(actual);
 		do {
 			System.out.println("Elige una opcion: \n\t1. Ver espectaculos\n\t.2 " + "Log IN\n\t3. Salir");
 			opcion = leer.nextInt();
@@ -33,6 +34,7 @@ public class Principal {
 			case 3:
 				break;
 			default:
+				System.out.println("No has introducido una opcion valida." + " Por favor intentalo de nuevo.");
 
 			}
 		} while (opcion != 3);
@@ -42,11 +44,51 @@ public class Principal {
 		 * 1. ver espectaculos 2. gestionar espectaculos 2.1 crear-modificar espectaculo
 		 * 2.2 crear-modificar numero 2.3 asignar artistas 3. Log OUT
 		 */
+		mostrarMenuSesion(actual);
+		do {
+			System.out.println("Elige una opcion: \n\t1. Ver espectaculos\n\t.2 "
+					+ "Crear o Modificar espectaculos\n\t3. Log OUT\n\t4. Salir");
+			opcion = leer.nextInt();
+			leer.nextLine();
+			switch (opcion) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("No has introducido una opcion valida." + " Por favor intentalo de nuevo.");
+			}
+
+		} while (opcion != 4);
 
 		// MENU ARTISTA
 		/**
 		 * 1. ver tu ficha 2. ver espectaculos 3. Log OUT
 		 */
+		mostrarMenuSesion(actual);
+		do {
+			System.out.println(
+					"Elige una opcion: \n\t1. Ver tu ficha\n\t2. Ver " + "espectaculos\n\t3. Log OUT\n\t4. Salir");
+			opcion = leer.nextInt();
+			leer.nextLine();
+			switch (opcion) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("No has introducido una opcion valida." + " Por favor intentalo de nuevo.");
+			}
+
+		} while (opcion != 4);
 
 		// MENU ADMIN
 		/**
@@ -55,6 +97,36 @@ public class Principal {
 		 * credenciales 3.1 registrar persona 3.2 asignar perfil y credenciales 3.3
 		 * gestionar datos artista-coordinador 4. Log OUT
 		 */
+		
+		mostrarMenuSesion(actual);
+		do {
+			System.out.println(
+					"Elige una opcion: \n\t1. Ver espectaculos"
+					+ "\n\t2. Gestionar espectaculos"
+					+ "\n\t3. Gestionar personas y credenciales"
+					+ "\n\t4. Log OUT"
+					+ "\n\t5. Salir");
+			opcion = leer.nextInt();
+			leer.nextLine();
+			switch (opcion) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default: System.out.println("No has introducido una opcion valida." + " Por favor intentalo de nuevo.");
+			}
+		}
+		while(opcion != 5);
+	}
+
+	public static void mostrarMenuSesion(Sesion actual) {
+		System.out.println("Menu " + actual.getPerfilActual() + ":");
 	}
 
 }
